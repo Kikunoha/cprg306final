9c7d97314d258b63f27daa069a30d5e15b41af6f
Finnley Cottam <finnley.cottam@edu.sait.ca>
2025-04-21 21:40:32 -0600

    fix

--- app/layout.js
+++ app/layout.js
@@ -26,12 +26,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
- 
-export { nextConfig }
+
